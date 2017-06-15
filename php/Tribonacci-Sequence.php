cd8b27591043a23c2a91dfdf68aa90115909b7dc
Jie <mjunzh@gmail.com>
2017-06-15 23:17:40 +0800

    better

--- php/Tribonacci-Sequence.php
+++ php/Tribonacci-Sequence.php
@@ -1,16 +1,10 @@
 <?php
 
 function tribonacci($signature, $n) {
-    $c = count($signature);
-    if($n < 3){
-        return array_slice($signature, 0, $n);
-    }
 
-    while($c < $n){
-        $newNumber = $signature[$c - 3] + $signature[$c - 2] + $signature[$c - 1 ];
-        array_push($signature, $newNumber);
-        $c++;
+    for($i = $n - 3 ; $i > 0 ; $i--){
+        $signature[] = array_sum(array_slice($signature, -3));
     }
+    return array_slice($signature, 0, $n);
 
-    return $signature;
 }
